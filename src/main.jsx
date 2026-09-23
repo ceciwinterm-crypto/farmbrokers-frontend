da3318fd3754cd5626ab42fd25a8157a4ba6e5e5
ceciwinterm-crypto <ceci.winterm@gmail.com>
2026-09-23 12:31:00 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import CRM from './CRM.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {window.location.hash === '#crm' ? <CRM /> : <App />}
   </React.StrictMode>,
 )
