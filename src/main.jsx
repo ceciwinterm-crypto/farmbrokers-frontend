82e6c3a65de645a8858daef062c4932aa1246e0b
ceciwinterm-crypto <ceci.winterm@gmail.com>
2026-09-24 10:06:09 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import CRM from './CRM.jsx'
+import CRM, { FormularioPropietario } from './CRM.jsx'
+
+const hash = window.location.hash
+let pantalla = <App />
+if (hash.startsWith('#propietario/')) pantalla = <FormularioPropietario token={hash.split('/')[1]} />
+else if (hash === '#crm') pantalla = <CRM />
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {window.location.hash === '#crm' ? <CRM /> : <App />}
+    {pantalla}
   </React.StrictMode>,
 )
